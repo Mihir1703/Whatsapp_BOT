80287d167f00aa5eb9d2ddb8af106e68709fca6b
Mihir1703 <mihirwaykole1703@yahoo.com>
2022-05-21 13:08:05 +0530

    Message delete karo abb

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const makeWASocket = require('@adiwajshing/baileys').default
-const { DisconnectReason, useSingleFileAuthState } = require('@adiwajshing/baileys')
+const { DisconnectReason, useSingleFileAuthState, makeInMemoryStore } = require('@adiwajshing/baileys')
 const { Boom } = require('@hapi/boom')
 const fs = require('fs')
+let arr = require('./exceptions/delete.json');
+let deleteM = new Map();
+
+for (let i = 0; i < arr.length; i++) {
+    deleteM.set(arr[i].id, true);
+}
 const handleCommands = require('./utils/commands');
 
+const store = makeInMemoryStore({})
+try{
+    fs.unlinkSync('./baileys_store.json');
+}catch(err){}
+store.readFromFile('./baileys_store.json')
+// saves the state to a file every 10s
+setInterval(() => {
+    store.writeToFile('./baileys_store.json')
+}, 10_000)
+
 async function connectToWhatsApp() {
+    setTimeout(() => {
+        fs.unlinkSync('./baileys_store.json')
+    }, 3600000);
     const { state, saveState } = useSingleFileAuthState('./auth_info_multi.json')
     const sock = makeWASocket({
         auth: state,
         printQRInTerminal: true
     })
     sock.ev.on('creds.update', saveState)
+    store.bind(sock.ev)
     sock.ev.on('connection.update', (update) => {
         const { connection, lastDisconnect } = update
         if (connection === 'close') {
@@ -24,7 +44,20 @@ async function connectToWhatsApp() {
             console.log('opened connection')
         }
     })
+    sock.ev.on('messages.delete', async (chat) => {
+        console.log(chat)
+    })
     sock.ev.on('messages.upsert', async (chat) => {
+        try {
+            if (chat.messages[0].message.hasOwnProperty('protocolMessage')) {
+                if (deleteM.get(chat.messages[0].key.participant.split(':')[0])) {
+                    const datas = await store.loadMessage(chat.messages[0].message.protocolMessage.key.remoteJid, chat.messages[0].message.protocolMessage.key.id);
+                    await sock.sendMessage(chat.messages[0].message.protocolMessage.key.remoteJid, { text: '' }, { quoted: datas })
+                }
+            }
+        } catch (err) {
+            console.log(err)
+        }
         try {
             chat = chat.messages[0];
             let message;
