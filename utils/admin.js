b688f1af80d6a10070b4592b27e6b09e033eb63e
Mihir Waykole <76737051+Mihir1703@users.noreply.github.com>
2022-06-23 09:07:01 +0000

    added advantage

--- utils/admin.js
+++ utils/admin.js
@@ -1,6 +1,12 @@
 const fs = require('fs')
-module.exports = async (client,controller,chat) => {
-    let data = JSON.parse(fs.readFileSync('./exceptions/who_can_tag.json',{encoding:'utf8'}))
+module.exports = async (client, controller, chat) => {
+    if (!chat.key.fromMe) {
+        await controller.sendMessage(client, { text: "Permission denied!!" }, {
+            quoted: chat
+        });
+        return;
+    }
+    let data = JSON.parse(fs.readFileSync('./exceptions/who_can_tag.json', { encoding: 'utf8' }))
     let data_add = [];
     chat.message.extendedTextMessage.contextInfo.mentionJid.map((e) => {
         data_add.push({
@@ -8,6 +14,6 @@ module.exports = async (client,controller,chat) => {
         })
     })
     data.push(...data_add)
-    fs.writeFileSync('./exceptions/who_can_tag.json',JSON.stringify(data));
+    fs.writeFileSync('./exceptions/who_can_tag.json', JSON.stringify(data));
     await controller.sendMessage(client, { text: "Access Granted!!" });
 }
